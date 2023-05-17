5d68441963ac19a38e17845f01ce9d04ad6ed7a9
Rayan Hantoul <ryan-055@outlook.com>
2023-05-18 00:31:07 +0300

    update

--- Coffee-Shop/Drinks_page.py
+++ Coffee-Shop/Drinks_page.py
@@ -13,7 +13,7 @@ price_cold_drinks = list(dict_cold_drinks.values())
 
 def hot_drinks():
     count = 1
-    print("Hot Drinks")
+    print("\n Hot Drinks")
 
     for i in range(len(name_hot_drinks)):
         print(f"{count}-{name_hot_drinks[i]} {price_hot_drinks[i]}.SR \n")
@@ -21,24 +21,37 @@ def hot_drinks():
 
 def cold_drinks():
     count = 1
-    print("Cold Drinks")
+    print("\n Cold Drinks")
     
     for i in range(len(name_cold_drinks)):
         print(f"{count}-{name_cold_drinks[i]} {price_cold_drinks[i]}.SR \n")
         count +=1
         
-def take_order(order):
-    if order in name_hot_drinks:
-        order_list.append(order)
-        price_list.append(dict_hot_drinks[order])
-        print("added to the invoice")
-        return
-    elif order in name_cold_drinks:
-        order_list.append(order)
-        price_list.append(dict_cold_drinks[order])
-        print("added to the invoice")
-        return
-        
-    else:
-        print("please check the name of order")
+def take_order(type:str,order:str):
+    if type.lower() == "h" or type.lower() == "hot": 
+        if order in name_hot_drinks:
+            order_list.append(order)
+            price_list.append(dict_hot_drinks[order])
+            print("added to the invoice")
+            return
+        else:
+            print("please check the name of order")
 
+    elif type.lower() == "c" or type.lower() == "cold":
+        if order in name_cold_drinks:
+            order_list.append(order)
+            price_list.append(dict_cold_drinks[order])
+            print("added to the invoice")
+            return
+        else:
+            print("please check the name of order")
+    else:
+        print("Please check the input!")
+        
+def print_your_order():
+    count = 1
+    print("\n You added this items ")
+    
+    for i in range(len(order_list)):
+        print(f"{count}-{order_list[i]} {price_list[i]}.SR \n")
+        count +=1
--- Coffee-Shop/Home_page.py
+++ Coffee-Shop/Home_page.py
@@ -9,37 +9,70 @@ print(wellcome)
 
 user_name = str(input("Hi Enter Your Name!: "))
 
-if user_name.isnumeric() or user_name.startswith(" "):
-     raise ValueError("You should enter a charcter only!")
+if user_name.isnumeric() or user_name.startswith(" ") or user_name == "":
+     raise ValueError("You should enter a character only!")
      
 else:
-   print(f"Wellcome {user_name} \n")
+   print(f"\n Wellcome {user_name} \n")
    
 print("")
-
-input_of_user = ""
 
 
 print("in this program you can order some coffee and dounts... \n")
 print("Look at the list of product \n \n")
+print("Coffee")
 Drinks_page.hot_drinks() , Drinks_page.cold_drinks()
 
+def process_order(answer:str):
 
-input_of_user = input("Do you want to order? y(yes) or n(no) ")
+    if answer.lower() == "n" or answer.lower() == "no":
+        return answer
 
-if input_of_user.lower() == "y" or "yes":
-    input_of_user = input("\n Drinks(d) Or Dounts(dn)? ")
+    answer = input("\n Drinks(d) Or Dounts(dn)? ")
 
-    if input_of_user.lower() == "d":
-        Drinks_page.hot_drinks()
-        input_of_user = input("write waht you want of list: ")
-        input_of_user = input_of_user.title()
-        Drinks_page.take_order(input_of_user)
+    if answer.lower() == "d" or answer.lower() == "drinks":
+        answer = input("\n Hot Drinks(h) Or Cold Drinks(c)")
+
+        if answer.lower() == "h" or answer.lower() == "hot":
+            Drinks_page.hot_drinks()
+            answer = input("\n write waht you want of the list: ")
+            answer = answer.title()
+            Drinks_page.take_order("h",answer)
+
+        elif answer.lower() == "c"  or answer.lower() == "cold":
+            Drinks_page.cold_drinks()
+            answer = input("\n write waht you want of the list: ")
+            answer = answer.title()
+            Drinks_page.take_order("c",answer)
+
+        else:
+            print("please check the answer!")
+            return process_order()
+
+        #elif input_of_user.lower() == "dn" or "dounts":
+        #    pass
+    answer = input("Do you want add anything else? ")
+    process_order(answer)   
 
 
-    elif input_of_user.lower() == "dn":
-        pass
-    
+user_answer = "no"
+while user_answer == "n" or user_answer == "no":
+
+    input_of_user = input("Do you want to order? y(yes) or n(no) ")
+
+    if input_of_user == "y":
+                
+        print(process_order(input_of_user))
+        Drinks_page.print_your_order()
+            #checkout
+            #then exit
+    else:
+        user_answer = input("Do you want to exit y(yes) or n(no)? ")
+
+        if user_answer == "y" or user_answer == "yes":
+            exit()
+        
+            
 
 #while input_of_user != "exit":
 #    pass
