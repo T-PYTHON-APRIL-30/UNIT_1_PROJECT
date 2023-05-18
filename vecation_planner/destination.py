a9803bc9dcbc841817cdf81c4767f50f20dc8711
Dalia <daliaasq@gmail.com>
2023-05-18 14:10:30 +0300

    changed some options add one function and saving file feature

--- vecation_planner/destination.py
+++ vecation_planner/destination.py
@@ -28,6 +28,17 @@ class Destination:
             self.attractions = ["Ithra Center", "Qarah Mount", "Heritage Village"]
             self.restaurants = ["Madina Restaurant","Shwait Alkhalij","Alayyad Abahry"]
             self.coffee_shops = ["llama cafe","UMQ Cafe","ANDES Coffee Roasters"]
+        
+        elif self.city == "AlAhsa": 
+            self.attractions = ["AlQarah", "Dougha Handmade Pottery Factory", "Jawatha Mosque","Ibrahim Palace","Souq Al Qaisariya","Uqair Beach","Um Sabah"]
+            self.restaurants = ["Dar Albasmah"]
+            self.coffee_shops = []
+
+        elif self.city == "Taif": 
+            self.attractions = ["Shubra Palace Museum", "Al Qahdi Rose Factory", "The Taif Souq","Daka Mountain Park","AlWahba Crater","Hiking in Taif"]
+            self.restaurants = []
+            self.coffee_shops = []
+        
 
     def plan_trip(self):
         for day in range(1, self.days+1):
@@ -36,24 +47,45 @@ class Destination:
             attractions = random.sample(self.attractions, k=1)
             restaurants = random.sample(self.restaurants, k=1)
             coffee_shops = random.sample(self.coffee_shops, k=1)
-            print("Activity of the day:")
+            print("- Activity of the day:")
             for attraction in attractions:
                 print(attraction)
                 print("\n")
 
-            print("Restaurant:")
+            print("- Restaurant:")
             for restaurant in restaurants:
                 print(restaurant)
                 print("\n")
 
-            print("Coffee of the day:")
+            print("- Coffee of the day:")
             for coffee in coffee_shops:
                 print(coffee)
                 print("\n")
 
+    def plan_experience(self):
+        itinerary = ""
+        for day in range(1, self.days+1):
+            itinerary +=f"\nDay {day} in {self.city}:\n"
+            itinerary +="-"*30 + "\n"
+            attractions = random.sample(self.attractions, k=3)
+            for attraction in attractions:
+                itinerary +="- " + attraction + "\n"
+                self.attractions.remove(attraction)
+            itinerary += "\n"
+        print(itinerary)
+        save_file = input("Do you want to save the itinerary to a text file? (Y/N) ")
+        if save_file.lower() == "y":
+            filename = input("Please enter a filename: ")
+            with open(filename, "w") as f:
+                f.write(itinerary)
+            print(f"Saved itinerary to {filename}.\n")
+        else:
+            print("Itinerary not saved.\n")
+                
 
-days = int(input("How many days you plan to stay? "))
-trip_planner = Destination("Riyadh", days)
-trip_planner.plan_trip()
+        
+
+
+
 
 
--- vecation_planner/main.py
+++ vecation_planner/main.py
@@ -1,13 +1,16 @@
 '''
 List to-do:
-ask for staying days and print suggested plan for each day
 Use a Lambda function.
-Use at least 1 Class.
 Use some form of Error Handling .
-Organize Your Code into modules & (or packages)
 design a nicer welcomming
 
+ask for staying days and print suggested plan for each day: DONE
+Use at least 1 Class: DONE
+Organize Your Code into modules & (or packages): DONE
+
 '''
+from destination import Destination
+
 def welcoming():
     print("\n")
     print("-"*40)
@@ -20,18 +23,23 @@ def welcoming():
 welcoming()
 
 cities=["Riyadh","Jeddah","Alula","Dammam"]
-atrractions ={"Riyadh":{"breakfast":"Chapter","activity":"Hiking","lunch":"Full of","coffee shop":"Mkth","dinner":"San Carlo","info":"In the ever-growing and flourishing city of Riyadh, you will discover the birthplace of the Kingdom of Saudi Arabia, along with its historical treasures hidden in the old palaces that witnessed the founding of the kingdom. It is a destination for tourists from all over the globe who wish to discover a world of shopping, entertainment and business. The malls offer the most exciting shopping experience, and the sand dunes combined with the brightest stars in the sky present the most magical experience in nature. It is an environment full of a natural variety and unique characteristics that are intriguing for explorers. On the other side of the bustling city, you can enjoy a variety of experiences in luxurious restaurants that offer their special services, and their elaborate international dishes. Simultaneously, the local Riyadh restaurants will tempt you with their authentic flavors and blend of spices originating from Saudi culture. Everyone is happy in the city of Riyadh where the biggest cultural events are always celebrated. Endless entertainment destinations are always emerging, for the city visitors to have intriguing and renewing experiences."},
+atrractions ={"Riyadh":{"breakfast":"Chapter","activity":"Hiking","lunch":"Full of","coffee shop":"Mkth","dinner":"San Carlo","info":""},
               "Jeddah":{"breakfast":"","activity":"","lunch":"","coffee shop":"","dinner":""},
               "Alula":{"breakfast":"","activity":"","lunch":"","coffee shop":"","dinner":""},
               "Dammam":{"breakfast":"","activity":"","lunch":"","coffee shop":"","dinner":""},}
 def trend_cities():
     #this function print the cities for user to choose and then print a suggested plan
-    print(cities)
+    for city in cities:
+        print("+ "+city)
     print("\n")
     user_input = input("choose a city: ")
+    print("\n")
     if user_input in cities:
         print(f"You chose a {user_input}")
-        print(f'- Breakfast:{atrractions["Riyadh"]["breakfast"]}\n- Activity of the day:{atrractions["Riyadh"]["activity"]}\n- Lunch:{atrractions["Riyadh"]["lunch"]}\n- Coffee of the day:{atrractions["Riyadh"]["coffee shop"]}\n- Dinner:{atrractions["Riyadh"]["dinner"]}\n')
+        days = int(input("How many days you plan to stay? "))
+        trip_planner = Destination(user_input, days)
+        trip_planner.plan_trip()
+        
 
 def view_all():
     for city in cities:
@@ -50,18 +58,41 @@ def search_city(city):
     else:
         print(f"This city not available yet\n")
 
-def city_info(city):
+'''def city_info(city):
     if city in cities:
         print(atrractions["Riyadh"]["info"])
     else:
-        print("This city not available yet")
+        print("This city not available yet")'''
+
+def exper_city():
+    num = 0
+    print("\nBest experiences and tours:")
+    print("1.Four days in Riyadh")
+    print("2.Two days in AlAhsa")
+    print("3.Two days in Taif")
+    print("4.One day in Jeddah")
+    print("\n")
+    num = int(input("Choose your experiences:"))
+    if num == 1:
+        days = 4
+        trip_planner = Destination("Riyadh", days)
+        trip_planner.plan_trip()
+    elif num == 2:
+        trip_planner = Destination("AlAhsa", 2)
+        trip_planner.plan_experience()
+    elif num == 3:
+        trip_planner = Destination("Taif", 2)
+        trip_planner.plan_experience()
+    elif num == 4:
+        trip_planner = Destination("Jeddah", 1)
+        trip_planner.plan_trip()
     
 
 
 choice = 0 
 while choice != 4:
-    print("1. Trending Destination:")
-    print("2. View All Destinations")
+    print("1. Start Plan Your Trip")
+    print("2. Best experiences and tours")
     print("3. Search a city")
     print("4. Quit\n")
     choice = int(input("Choose an option: "))
@@ -69,13 +100,9 @@ while choice != 4:
     if choice == 1:
         print("\nTrending Destination: ")
         trend_cities()
+
     elif choice == 2:
-        print("\nAll Destinations:")
-        view_all()
-        input_city = input("\nChoose a city: ")
-        choose_city(input_city)
-        city_info(input_city)
-        #print information about the choosen city 
+        exper_city()
         #print suggested attractions
         #Do you want to add the plan?
         # if yes save it in a txt file
