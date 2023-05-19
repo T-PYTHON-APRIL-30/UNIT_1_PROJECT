00961e58f4ff4cef1bfde9d5d943be9d3798edd7
saud <sasud000@gmail.com>
2023-05-19 21:36:05 +0300

    fixed the methods

--- SearchPhone.py
+++ SearchPhone.py
@@ -3,36 +3,51 @@ from time import sleep
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
-#set a browser
-options = Options()
-profile = webdriver.FirefoxProfile()
-profile.set_preference("browser.privatebrowsing.autostart", True)
-options.add_argument('-headless')
-options.binary_location = r"C:\Program Files\Mozilla Firefox/firefox.exe"
-driver = webdriver.Firefox(options=options, firefox_profile=profile, executable_path="C:/geckodriver.exe")
-driver.implicitly_wait(2)
-driver.delete_all_cookies()
-sleep(0.5)
-
 class Checkphone:
-
     def __init__(self,phone):
         self.phone= phone
+        # set a browser
+        options = Options()
+        profile = webdriver.FirefoxProfile()
+        profile.set_preference("browser.privatebrowsing.autostart", True)
+        options.add_argument('-headless')
+        options.binary_location = r"C:\Program Files\Mozilla Firefox/firefox.exe"
+        self.driver = webdriver.Firefox(options=options, firefox_profile=profile, executable_path="C:/geckodriver.exe")
+        self.driver.implicitly_wait(2)
+        self.driver.delete_all_cookies()
+        sleep(0.5)
 
-
-    def search_name(self):
-        driver.get('http://ksanumbers.pro/')
-        driver.find_element(By.NAME,"num").send_keys(self.phone)
+    def twitter_checker(self):
+        self.driver.get('https://twitter.com/i/flow/signup')
         sleep(0.2)
-        driver.find_element(By.XPATH,'//*[@id="sbtn"]/i').click()
+        self.driver.find_element(By.XPATH,"/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/div/span/span").click()
+        sleep(0.3)
+        post_number = self.driver.find_element(By.XPATH,"/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/label/div/div[2]/div/input")
+        post_number.send_keys(self.phone)
         sleep(0.2)
         try:
-            name = driver.find_element(By.ID, 'result').text
+            message = self.driver.find_element(By.XPATH,"/html/body/div/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/div[2]/div/div/div/div/span")
+            used = "This number is already in use with other accounts. Please use another."
+            if message.text == used:
+                print(f"Twitter : Found...")
+
+        except:
+            print("Twitter : Not Found ...")
+
+
+    def name_checker(self):
+        self.driver.get('http://ksanumbers.pro/')
+        self.driver.find_element(By.NAME,"num").send_keys(self.phone)
+        sleep(0.2)
+        self.driver.find_element(By.XPATH,'//*[@id="sbtn"]/i').click()
+        sleep(0.2)
+        try:
+            name = self.driver.find_element(By.ID, 'result').text
             if name == name:
-                print(f"Name is : {name}")
+                print(f"Name is : {name}\n")
 
             else:
-                no_result = driver.find_element(By.XPATH, "/html/body/main/div/div/div/div[2]/div/li")
+                no_result = self.driver.find_element(By.XPATH, "/html/body/main/div/div/div/div[2]/div/li")
                 message = "لا يوجد نتائج"
                 if no_result.text == message:
                     return no_result.text
@@ -40,23 +55,57 @@ class Checkphone:
         except Exception as e:
             print(f"An error occurred: {e}")
 
-    def search_facebook(self):
-        global driver
-        driver.get("https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0")
-        put_Phone= driver.find_element(By.ID,'identify_email')
+    def facebook_checker(self):
+        self.driver.get("https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0")
+        put_Phone= self.driver.find_element(By.ID,'identify_email')
         put_Phone.send_keys(self.phone)
         sleep(0.3)
-        driver.find_element(By.ID,"did_submit").click()
+        self.driver.find_element(By.ID,"did_submit").click()
         sleep(0.2)
         #try to check if this number is sign up or available
         try:
             error= "No Search Results"
-            chek= driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[1]/div/div[2]/div/div/form/div/div[2]/div[1]/div[1]").text
+            chek= self.driver.find_element(By.XPATH,"/html/body/div[1]/div[1]/div[1]/div/div[2]/div/div/form/div/div[2]/div[1]/div[1]").text
             if chek == error:
                 print(f"FaceBook : Not Found ...")
                 sleep(0.2)
         except:
             print(f"\nFaceBook : Found...")
+        self.driver.close()
+
+    def microsoft_checker(self):
+        url = "https://login.live.com/login.srf?wa=wsignin1.0&rpsnv=13&ct=1684318779&rver=7.0.6737.0&wp=MBI_SSL&wreply=https%3a%2f%2foutlook.live.com%2fowa%2f%3fexch%3d1%26nlp%3d1%26RpsCsrfState%3d10bf6aa2-c57c-f089-665e-cc07c700cfc8&id=292841&aadredir=1&CBCXT=out&lw=1&fl=dob%2cflname%2cwld&cobrandid=90015"
+        self.driver.get(url)
+        sleep(0.3)
+        keys = self.driver.find_element(By.XPATH, '//*[@id="i0116"]')
+        keys.send_keys(self.phone)
+        sleep(0.3)
+        try:
+            next = self.driver.find_element(By.XPATH, "//*[@id='idSIButton9']")
+            next.click()
+            sleep(2)
+            confirm = self.driver.find_element(By.XPATH, '//*[@id="loginHeader"]').text
+            textconfi = "Confirm your phone number"
+            if confirm == textconfi:
+                next = self.driver.find_element(By.XPATH, "//*[@id='idSIButton9']")
+                next.click()
+                sleep(2)
+            else:
+                print("")
+            sleep(0.9)
+            result = self.driver.find_element(By.XPATH, '//*[@id="usernameError"]').text
+            usererror = "This phone number does not exist as a username. Please check if your number is correct."
+            if result == usererror:
+                print(f"Microsoft : Not Found ... \n")
+        except:
+            auth = self.driver.find_element(By.XPATH, '//*[@id="loginHeader"]').text
+            res = "Check Microsoft Authenticator"
+            if auth == res:
+                print(f" Microsoft : Found ...")
+            else:
+                print(f"Microsoft : Found... \n")
+        sleep(0.2)
+        self.driver.quit()
 
 
 
--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 from SearchPhone import Checkphone
-from SocialTracker import TwitterChecker, microsoft_checker
 from art import *
 from termcolor import colored
 
+#ascii style
+
 def print_text_art(text, font, color):
-    text_art = text2art(text, font)
+    text_art = text2art(text, font='small')
     colored_text_art = colored(text_art, color)
     print(colored_text_art)
 
-print_text_art("PhoneChecker ", "block", 'red')
+print_text_art("PhoneNum-Checker ", "starwars", 'red')
+
+#User InterFace
+print("Please select an option:\n")
+print("1. Search for a specific phone number.")
+print("2. Search for multiple phone numbers.")
+option = int(input("Enter your option (1 or 2): \n"))
+try:
+
+    if option == 1:
+        Singlphone=(input("Enter the phone number:-  "))
+        userinput=Checkphone(Singlphone.split())
+        print(f"Phone Number is : {Singlphone}\n")
+        userinput.name_checker()
+        userinput.twitter_checker()
+        userinput.facebook_checker()
+        userinput1 = Checkphone(Singlphone[3:])
+        userinput1.microsoft_checker()
 
 
-choose= int(input("please choose 1 if you want search a single number ,, or  2 if you want upload multi number: -"))
-if choose == 1:
-    Singlphone=(input("Please Provide a Phone Number :- "))
-    userinput=Checkphone(Singlphone.split())
-    print(f"Phone Number is : {Singlphone}\n")
-    userinput.search_name()
-    userinput.search_facebook()
-    TwitterChecker.search_twitter(Singlphone)
-    microsoft_checker.Microsofot_checker(Singlphone[3:])
-elif choose == 2:
-    phone_checker = open('phone.txt','r+',encoding='utf-8')
-    for line in phone_checker:
-        print(f"Phone Number is : {line.split()}\n")
-        face =Checkphone(line.split())
-        face.search_name()
-        face.search_facebook()
-        TwitterChecker.search_twitter(line.split())
-        microsoft_checker.Microsofot_checker(line[3:])
-        print("---"*5)
 
-else:
-    exit()
+    elif option == 2:
+        phone_checker = open('phone.txt','r+',encoding='utf-8')
+        for line in phone_checker:
+            print(f"Phone Number is : {line}\n")
+            multi =Checkphone(line.split())
+            multi.name_checker()
+            multi.twitter_checker()
+            multi.facebook_checker()
+            cut_num= Checkphone(line[3:])
+            cut_num.microsoft_checker()
+            print("---"*5)
+
+    else:
+        exit()
+except Exception as e:
+    print(f"An error occurred: {e}")
